0afa54887ea9b9265e46a5b5627948168959efb7
Jeff H <jeffrey92@gmail.com>
2018-06-10 14:17:35 -0700

    pwhash stub

---
+++ pwhash.go
@@ -0,0 +1,47 @@
+package hydrogen
+
+// #cgo CFLAGS: -Ilibhydrogen
+// #cgo LDFLAGS: -Llibhydrogen -lhydrogen
+// #include <stdlib.h>
+// #include <hydrogen.h>
+import "C"
+
+// import (
+// 	"unsafe"
+// )
+
+const (
+	PwHashContextBytes   int = C.hydro_pwhash_CONTEXTBYTES
+	PwHashMasterKeyBytes int = C.hydro_pwhash_MASTERKEYBYTES
+	PwHashStoredBytes int = C.hydro_pwhash_STOREDBYTES
+)
+
+// void hydro_pwhash_keygen(uint8_t master_key[hydro_pwhash_MASTERKEYBYTES]);
+func PwHashKeygen() []byte {
+	out := make([]byte, PwHashMasterKeyBytes)
+	C.hydro_pwhash_keygen((*C.uchar)(&out[0]))
+	return out
+}
+
+
+//
+// TODO
+//
+
+
+// int hydro_pwhash_deterministic(uint8_t *h, size_t h_len, const char *passwd, size_t passwd_len, const char ctx[hydro_pwhash_CONTEXTBYTES], const uint8_t master_key[hydro_pwhash_MASTERKEYBYTES], uint64_t opslimit, size_t memlimit, uint8_t threads);
+
+// int hydro_pwhash_create(uint8_t stored[hydro_pwhash_STOREDBYTES], const char *passwd, size_t passwd_len, const uint8_t master_key[hydro_pwhash_MASTERKEYBYTES], uint64_t opslimit, size_t memlimit, uint8_t threads);
+
+// int hydro_pwhash_verify(const uint8_t stored[hydro_pwhash_STOREDBYTES], const char *passwd, size_t passwd_len, const uint8_t master_key[hydro_pwhash_MASTERKEYBYTES], uint64_t opslimit_max, size_t memlimit_max, uint8_t threads_max);
+
+// int hydro_pwhash_derive_static_key(uint8_t *static_key, size_t static_key_len, const uint8_t stored[hydro_pwhash_STOREDBYTES], const char *passwd, size_t passwd_len, const char ctx[hydro_pwhash_CONTEXTBYTES], const uint8_t master_key[hydro_pwhash_MASTERKEYBYTES], uint64_t opslimit_max, size_t memlimit_max, uint8_t threads_max);
+
+// int hydro_pwhash_reencrypt(uint8_t stored[hydro_pwhash_STOREDBYTES], const uint8_t master_key[hydro_pwhash_MASTERKEYBYTES], const uint8_t new_master_key[hydro_pwhash_MASTERKEYBYTES]);
+
+// int hydro_pwhash_upgrade(uint8_t stored[hydro_pwhash_STOREDBYTES], const uint8_t master_key[hydro_pwhash_MASTERKEYBYTES], uint64_t opslimit, size_t memlimit, uint8_t threads);
+
+
+//
+// eof
+//
--- tests/main.go
+++ tests/main.go
@@ -12,6 +12,7 @@ func main() {
 	fmt.Println("finish")
 	ExampleHash()
 	ExampleKdf()
+	ExamplePwHash()
 	ExampleRandom()
 	ExampleSecretbox()
 	ExampleSign()
@@ -25,7 +26,7 @@ const TEST_MSG1 = "testing 123"
 const TEST_MSG2 = "testing abc"
 
 func ExampleHash() {
-	fmt.Println("============= Hash =============")
+	fmt.Printf("\n============= Hash =============\n")
 	fmt.Printf("HashBytes = %d\n", hydro.HashBytes)
 	fmt.Printf("HashBytesMax = %d\n", hydro.HashBytesMax)
 	fmt.Printf("HashBytesMin = %d\n", hydro.HashBytesMin)
@@ -52,21 +53,35 @@ func ExampleHash() {
 }
 
 func ExampleKdf() {
-	fmt.Println("============= Kdf =============")
+	fmt.Printf("\n============= Kdf =============\n")
 	fmt.Printf("KdfContextBytes = %d\n", hydro.KdfContextBytes)
 	fmt.Printf("KdfKeyBytes = %d\n", hydro.KdfKeyBytes)
 	fmt.Printf("KdfMaxBytes = %d\n", hydro.KdfMaxBytes)
 	fmt.Printf("KdfMinBytes = %d\n", hydro.KdfMinBytes)
-	fmt.Println("KdfKeygen:")
+
+	fmt.Printf("\n--- KdfKeygen ---\n")
 	master := hydro.KdfKeygen()
 	fmt.Printf("master [%d] %s\n", len(master), hydro.Bin2hex(master))
+
+	fmt.Printf("\n--- KdfDeriveFromKey ---\n")
 	var id uint64 = 0x0123456789ABCDEF
 	subkey, _ := hydro.KdfDeriveFromKey(32, id, GOOD_CTX, master)
 	fmt.Printf("subkey [%d] %s\n", len(subkey), hydro.Bin2hex(subkey))
 }
 
+func ExamplePwHash() {
+	fmt.Printf("\n============= PwHash =============\n")
+	fmt.Printf("PwHashContextBytes = %d\n", hydro.PwHashContextBytes)
+	fmt.Printf("PwHashMasterKeyBytes = %d\n", hydro.PwHashMasterKeyBytes)
+	fmt.Printf("PwHashStoredBytes = %d\n", hydro.PwHashStoredBytes)
+
+	fmt.Printf("\n--- PwHashKeygen ---\n")
+	master := hydro.PwHashKeygen()
+	fmt.Printf("master [%d] %s\n", len(master), hydro.Bin2hex(master))
+}
+
 func ExampleRandom() {
-	fmt.Println("============= Random =============")
+	fmt.Printf("\n============= Random =============\n")
 	fmt.Printf("RandomSeedBytes = %d\n", hydro.RandomSeedBytes)
 
 	fmt.Printf("\n--- RandomU32 ---\n")
@@ -87,7 +102,7 @@ func ExampleRandom() {
 }
 
 func ExampleSecretbox() {
-	fmt.Println("============= Secretbox =============")
+	fmt.Printf("\n============= Secretbox =============\n")
 	fmt.Printf("SecretboxContextBytes = %d\n", hydro.SecretboxContextBytes)
 	fmt.Printf("SecretboxHeaderBytes = %d\n", hydro.SecretboxHeaderBytes)
 	fmt.Printf("SecretboxKeyBytes = %d\n", hydro.SecretboxKeyBytes)
@@ -117,7 +132,7 @@ func ExampleSecretbox() {
 }
 
 func ExampleSign() {
-	fmt.Println("============= Sign =============")
+	fmt.Printf("\n============= Sign =============\n")
 	fmt.Printf("SignBytes = %d\n", hydro.SignBytes)
 	fmt.Printf("SignContextBytes = %d\n", hydro.SignContextBytes)
 	fmt.Printf("SignPublicKeyBytes = %d\n", hydro.SignPublicKeyBytes)
